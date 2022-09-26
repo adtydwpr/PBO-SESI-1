4552d85a2e686c3a907ddf89616c10eae104d951
adtydwpr <114389480+adtydwpr@users.noreply.github.com>
2022-09-26 15:04:17 +0700

    Create soal2.java

---
+++ soal2.java
@@ -0,0 +1,11 @@
+import javax.swing.JOptionPane;
+
+public class soal2 {
+
+  
+    public static void main(String[] args) {
+        String namaTeman = JOptionPane.showInputDialog("Anda sedang belajar apa? ");
+        JOptionPane.showMessageDialog(null, "Belajar "+namaTeman+" Sangat mudah   ");
+    }
+
+}
